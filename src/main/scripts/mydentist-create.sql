098c476b3d935f5257d03322582052524d0b43fd
vallabhnaik <vallabhnaik16@gmail.com>
2016-10-23 11:20:12 -0700

    Patient Home Page successfully created

--- src/main/scripts/mydentist-create.sql
+++ src/main/scripts/mydentist-create.sql
@@ -1,16 +1,1 @@
-create sequence hibernate_sequence start 1 increment 1;
-
-    
-
-    create table users (
-        userId int4 not null,
-        userEmail varchar(255),
-        userPassword varchar(255),
-        userType varchar(255),
-        username varchar(255),
-        primary key (userId)
-    );
-    
-    
-
-    
+insert into users values('5','tom','cat','patient','tom','abcd 1234','12412','1991-12-12')
