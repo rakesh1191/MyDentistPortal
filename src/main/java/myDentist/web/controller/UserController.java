ed13b4cef4770f5df44fc1ec8c5c5bc6fd2d971c
Sanket Sonawane <sanket176@gmail.com>
2016-11-05 14:30:38 -0700

    Update UserController.java

--- src/main/java/myDentist/web/controller/UserController.java
+++ src/main/java/myDentist/web/controller/UserController.java
@@ -1,11 +1,17 @@
 package myDentist.web.controller;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.servlet.http.HttpSession;
 
 import org.hibernate.Session;
 import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -46,61 +52,13 @@ public class UserController {
 		return "display";
 	}
 	
-	@RequestMapping("/logout.html")
-	public String loginOut(HttpSession session)
-	{	
-		//if(session!=null)
-		session.invalidate();
-		//models.put("users", userDao.getUsers());
-		return "logout.html";
-	}
+	
 	
 	@RequestMapping("/adminHome.html")
 	public String adminHome(ModelMap models)
 	{
 		models.put("appointments", appointmentsDao.getAppointments());
 		return "adminHome";
-	}
-	
-	@RequestMapping(value="/loginPage.html", method=RequestMethod.GET)
-	public String loginPage(ModelMap models)
-	{	
-		models.put("users", userDao.getUsers());
-		return "loginPage";
-	}
-	int uid=0;
-	@RequestMapping(value="/loginPage.html", method=RequestMethod.POST)
-	public String loginPage(@ModelAttribute ("user") User user,BindingResult result, ModelMap models,SessionStatus status)
-	{	
-		System.out.println(user.getUsername());
-		System.out.println(user.getUserPassword());
-		System.out.println(user.getUserType());
-		
-		for(User u : userDao.getUsers()){
-			if(user.getUsername().equals(u.getUsername())&& user.getUserPassword().equals(u.getUserPassword())){
-				if(u.getUserType().equals("patient")){
-					models.put("userid", u.getUserId());
-					//uid=u.getUserId();
-					status.setComplete();
-					return "redirect:PatientHome.html";
-				}else if(u.getUserType().equals("doctor")){
-					models.put("userid", u.getUserId());
-					uid=u.getUserId();
-					status.setComplete();
-					return "redirect:doctorHome.html";
-				}else if(u.getUserType().equals("admin")){
-					uid=u.getUserId();
-					status.setComplete();
-					return "redirect:adminHome.html";
-				}
-			}
-				
-		}
-		if(!result.hasErrors()){
-			return "loginPage.html";
-		}
-		
-		return "loginPage.html";
 	}
 	
 	
@@ -117,8 +75,12 @@ public class UserController {
 	{	
 		user.setUserType("patient");
 		// save user to database
+		Set<String> roles =new HashSet<String>();
+		roles.add("ROLE_USER");
+		user.setRoles(roles);
 		user=userDao.saveUser(user);
 		System.out.println("Data saved in db :"+user.getUsername());
+		
 		//redirect to display page
 		return "redirect:loginPage.html";
 	}
@@ -138,7 +100,7 @@ public class UserController {
 		user=userDao.saveUser(user);
 		System.out.println("Data saved in db :"+user.getUsername());
 		//redirect to display page
-		return "redirect:doctorHome.html";
+		return "redirect:loginPage.html";
 	}
 	
 	@RequestMapping(value="/editPatient.html", method=RequestMethod.GET)
@@ -154,12 +116,43 @@ public class UserController {
 	{	
 		User u=userDao.getUser(userid);
 		user.setUsername(u.getUsername());
-		user.setUserPassword(u.getUserPassword());
+		user.setPassword(u.getPassword());
 		user.setUserType(u.getUserType());
 		user.setDateOfBirth(u.getDateOfBirth());
 		user.setUserId(userid);
 		user=userDao.saveUser(user);
-		return "redirect:PatientHome.html?userid="+userid;
+		return "redirect:/users/profile.html?userid="+userid;
 	}
 	
+	@RequestMapping(value="/users/editUser.html", method=RequestMethod.GET)
+	public String editUser(ModelMap models,@RequestParam Integer userid)
+	{	
+		User u=userDao.getUser(userid);
+		models.put("alluser", u);
+		models.put("userid", userid);
+		return "/users/editUser";
+	}
+	
+
+	@RequestMapping(value="/users/editUser.html", method=RequestMethod.POST)
+	public String editUser(@RequestParam Integer userid,@RequestParam(required=false) String Enable,@RequestParam(required=false) String Disable)
+	{	
+		User u=userDao.getUser(userid);
+		if(Enable!=null){
+			u.setEnabled(true);
+			Set<String> roles =new HashSet<String>();
+			if(u.getUserType().matches("patient")){
+				roles.add("ROLE_USER");
+			}else if(u.getUserType().matches("doctor")){
+				roles.add("ROLE_DOCTOR");
+			}else{
+				roles.add("ROLE_ADMIN");
+			}
+			u.setRoles(roles);
+		}else{
+			u.setEnabled(false);
+		}
+		userDao.saveUser(u);
+		return "redirect:/users/profile.html?userid="+userid;
+	}
 }
